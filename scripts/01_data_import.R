4ca18a003c330942876cd7f3ccf6e109bdb7cab7
Mohamed Abdirashid-TECH <p9601679@gmail.com>
2026-05-17 14:52:37 +0300

    Add files via upload

---
+++ scripts/01_data_import.R
@@ -0,0 +1,16 @@
+# =========================================================
+# GLOBAL SUPERSTORE SALES ANALYSIS PROJECT
+# =========================================================
+#
+# Author : Mohamed Abdirashid Mohamud
+# Tool   : R & RStudio
+# Project: E-Commerce Sales Analysis
+#
+# Description:
+# This project analyzes Global Superstore sales data
+# to generate business insights, dashboards,
+# and performance analytics.
+#
+# =========================================================
+
+
